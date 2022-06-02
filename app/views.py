001c52a04598ae2745f5c3ffc7b8b36a02a97ac1
Agnes <tingyun2001@gmail.com>
2022-06-02 18:25:06 +0800

    add web pages

--- app/views.py
+++ app/views.py
@@ -15,9 +15,9 @@ from collections import Counter
 #from . import db
 #用MongoDB上傳檔案
 import openpyxl
-import pymongo
-from pymongo import MongoClient #使用mongodb
-import certifi #為了解決連線到SSL的問題
+#import pymongo
+#from pymongo import MongoClient #使用mongodb
+#import certifi #為了解決連線到SSL的問題
 import pathlib #分割副檔名
 import csv
 #import pandas as pd 重複了
@@ -41,19 +41,7 @@ def allowed_excel(filename):
     if ext.upper() in app.config["ALLOWED_EXCEL_EXTENSIONS"]:
         return True
     else:
-        return False
-
-#將檔案上傳到MongoDB
-def import_files_to_mongodb(filename):
-    conn = MongoClient("mongodb+srv://root:root159258@cluster0.oe4sl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tlsCAFile=certifi.where())
-    db = conn.test #選擇操作 test 資料庫
-    collection = db.test2 #選擇操作 users 集合
-    # test if connection success
-    collection.stats  # 如果沒有error，即就連線成功
-
-    data = pd.read_csv(app.config["EXCEL_UPLOADS"]+"/"+filename, encoding = 'utf-8-sig') #從app.config["EXCEL_UPLOADS"]裡的位置抓到上傳的檔案
-    data_json = json.loads(data.to_json(orient='records'))
-    collection.insert_many(data_json)                 
+        return False                
 
 app.config["EXCEL_UPLOADS"] = "/app/app/static/excel" #儲存位置
 app.config["ALLOWED_EXCEL_EXTENSIONS"] = ["XLSX", "XLS", "XML", "XLT", "CSV"] #允許的副檔名
@@ -90,28 +78,13 @@ def upload_excel():
                     str_upload_path = str(app.config["EXCEL_UPLOADS"])
 
                     os.rename(str_upload_path + "/" + excel.filename,str_upload_path + "/" + "ori.xlsx")
-                    #將excel檔案儲存在資料夾
-                    #excel.save(os.path.join(app.config["EXCEL_UPLOADS"], filename))
-                    #讀取使用者上傳的excel檔
-                    #data_xls = pd.read_excel(app.config["EXCEL_UPLOADS"]+"/"+filename, index_col=0)
-
-                    #由excel轉換的csv固定名稱為original.csv
-                    #data_xls.to_csv(app.config["EXCEL_UPLOADS"]+"/"+'original.csv', encoding='utf-8-sig')
-                    #import_file_to_mongodb('original.csv')
-
-                    #由excel轉換的csv名稱為 原檔名.csv
-                    #取出檔名(不要副檔名)
-                    #filename_NoExt = os.path.splitext(filename)[0] 
-                    #將 檔名.xlsx 轉換為 檔名.csv，並儲存在app.config["EXCEL_UPLOADS"]裡
-                    #data_xls.to_csv(app.config["EXCEL_UPLOADS"]+"/"+filename_NoExt + '.csv', encoding='utf-8-sig') #原本是encoding='utf-8'，但下載會變成亂碼，要改encoding='utf-8-sig'
-                    #import_files_to_mongodb(filename_NoExt + '.csv')
 
                 flash('Excel saved', 'success')
                 return redirect(request.url)
                 #return redirect("/download/"+filename) #會下載剛剛上傳的檔案
 
             else:
-                flash('請上傳附檔名為".xlsx .xls .xml .xlt .csv"的檔案', 'warning')
+                flash('請上傳附檔名為".xlsx .xls .xml .xlt"的檔案', 'warning')
                 return redirect(request.url)
 
     return render_template("public/upload_excel.html")
@@ -148,7 +121,7 @@ def test():
 
 
 #下載檔案，用from flask import send_from_directory, abort
-app.config["CLIENT_EXCELS"] = "/app/app/static/excel" #要從哪裡下載
+app.config["CLIENT_EXCELS"] = "/app/app/static/new_excel" #要從哪裡下載
 
 @app.route("/download/<excel_name>")
 def downloadfile(excel_name):
@@ -157,17 +130,3 @@ def downloadfile(excel_name):
     except FileNotFoundError:
         abort(404)
 #原本的程式碼return send_from_directory(app.config["CLIENT_EXCELS"], filename=excel_name, as_attachment=True)，現在filename要改成path
-
-
-
-#將csv檔上傳到MongoDB
-#conn = MongoClient("mongodb+srv://root:root159258@cluster0.oe4sl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tlsCAFile=certifi.where())
-#db = conn.test #選擇操作 test 資料庫
-#collection = db.users #選擇操作 users 集合
-
-# test if connection success
-#collection.stats  # 如果沒有error，即就連線成功
-
-#df = pd.read_csv("test.csv") #選擇要操作的csv檔
-#data = df.to_dict(orient = "records")
-#collection.insert_many(data) #新增資料
